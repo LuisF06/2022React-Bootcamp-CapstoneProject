31dd0aa1f8bed323affe226439f3935789449336
LuisF06 <luisfalvarezguerrero@gmail.com>
2022-06-05 11:23:29 -0500

    Added Initial Slider Implementation
    - Added Dummy Slider

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,9 @@
-//import Categories from "../../components/Categories/Categories";
-//import FeaturedProducts from "../../components/FeaturedProducts/FeaturedProducts";
-//import Slider from "../../components/Slider/Slider";
+import Slider from "../../components/Slider";
 
 const Home = () => {
   return (
     <div>
-      Home
+      <Slider/>
     </div>
   );
 };
