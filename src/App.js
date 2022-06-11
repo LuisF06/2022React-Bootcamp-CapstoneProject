292cb905a051df76c04d17ab8ca8b4cf176a63d7
LuisF06 <luisfalvarezguerrero@gmail.com>
2022-06-10 20:25:24 -0500

    Taking care of Review Findings

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import Footer from "./components/Footer";
 import StoreHeader from "./components/StoreHeader";
 import Home from "./pages/Home/Home";
-import { useFeaturedBanners } from "./utils/hooks/useFeaturedBanners";
 import {BrowserRouter as Router} from "react-router-dom"
 
 function App() {
-  const { data, isLoading } = useFeaturedBanners();
-  console.log(data, isLoading);
-
+  
   return (
     <>
       <Router>
